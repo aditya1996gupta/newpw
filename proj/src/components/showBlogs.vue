8e38c484aab40821bb1ed900edc8a3da9fde2a3b
ADITYA KUMAR GUPTA <30798959+aditya1996gupta@users.noreply.github.com>
2020-06-05 21:09:52 +0530

    Add files via upload

---
+++ proj/src/components/showBlogs.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div v-for="blog in filteredBlogs" class="single-blog" :key="blog">
+      {{ blog.text }}
+      <h2>{{ blog.title | uppercase }}</h2>
+      <article>{{ blog.body | snippet }}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: [],
+      search: ""
+    };
+  },
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+        this.blogs = data.body.slice(0, 10);
+      });
+  },
+  computed: {
+    filteredBlogs: function() {
+      return this.blogs.filter(blog => {
+        return blog.title.match(this.search);
+      });
+    }
+  }
+};
+</script>
+
+<style>
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
